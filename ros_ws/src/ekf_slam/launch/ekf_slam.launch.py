31bb2a6de4c2f4e2f302ef2cc7336804ac854660
dokyun-kim4 <donny5557@gmail.com>
2026-03-07 18:31:04 +0000

    add rviz config

--- ros_ws/src/ekf_slam/launch/ekf_slam.launch.py
+++ ros_ws/src/ekf_slam/launch/ekf_slam.launch.py
@@ -1,26 +1,35 @@
 import os
-
+from ament_index_python import get_package_share_directory
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
-
+    pkg_dir = get_package_share_directory('ekf_slam')
+    rviz_config_file = os.path.join(pkg_dir, 'config.rviz')
     return LaunchDescription([
         Node(
             package='ekf_slam',
-            executable='beacon'),
+            executable='beacon',
+            parameters=[{'use_sim_time': True}]
+        ),
 
         Node(
             package='ekf_slam',
-            executable='noise_injector'),
+            executable='noise_injector',
+            parameters=[{'use_sim_time': True}]
+        ),
 
         Node(
             package='ekf_slam',
-            executable='path_viz'
+            executable='path_viz',
+            parameters=[{'use_sim_time': True}]
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_file],
+            parameters=[{'use_sim_time': True}]
         )
     ])
--- ros_ws/src/ekf_slam/setup.py
+++ ros_ws/src/ekf_slam/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['launch/ekf_slam.launch.py']),
+        ('share/' + package_name, ['rviz/config.rviz']),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
