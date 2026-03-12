d0e8c6990a97a41e005d130477144ff1c542faeb
dokyun-kim4 <donny5557@gmail.com>
2026-03-12 06:59:08 +0000

    modify bag record script to take filename as argument

--- bag_record.sh
+++ bag_record.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+filename=$1
+
 cd /ekf-slam/ros_ws
 . install/setup.bash
-ros2 bag record -o test2 /ground_truth /beacon_ground_truth /beacon_noisy /encoder_noisy /gps_noisy /robot_description /tf /tf_static
+ros2 bag record -o rosbag/$filename /ground_truth /beacon_ground_truth /beacon_noisy /encoder_noisy /gps_noisy /robot_description /tf /tf_static
